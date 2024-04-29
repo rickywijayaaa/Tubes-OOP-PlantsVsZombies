d177dfbff1123059d9ceeed7e050dc98202961c5
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2024-04-29 11:11:18 +0700

    coba coba

---
+++ app/src/main/java/Dosen/Windy.java
@@ -0,0 +1,9 @@
+package app.src.main.java.Dosen;
+
+public class Windy {
+    public Windy(){};
+
+    public void printNama(){
+        System.out.println("NAMA SAYA GAMBETTA");
+    }
+}
---
+++ app/src/main/java/Plant/Plant.java
@@ -0,0 +1,10 @@
+package app.src.main.java.Plant;
+
+
+import app.src.main.java.Dosen.*;
+public class Plant {
+
+
+    Windy w = new Windy();
+    
+}
