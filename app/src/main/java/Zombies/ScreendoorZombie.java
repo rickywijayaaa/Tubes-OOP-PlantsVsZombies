77756dfce173a5cdd62bc5f2af8e62e19cc0e4e3
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2024-05-04 16:43:13 +0700

    update zombie

---
+++ app/src/main/java/Zombies/ScreendoorZombie.java
@@ -0,0 +1,33 @@
+package app.src.main.java.Zombies;
+
+import app.src.main.java.Creature.*;
+
+
+public class ScreendoorZombie extends Zombie {
+    public ScreendoorZombie(String name, int health, int attackDamage, double attackSpeed, boolean isAquatic, double slowed, double speed) {
+        super(name, health, attackDamage, attackSpeed, isAquatic, slowed, speed);
+    }
+
+
+
+    // Implementasi abstract method 
+    @Override
+    public void resetCooldown(){
+        // Implementasi resetCooldown dari Creature
+        System.out.println("nanti dioveerride");
+    }
+
+    // Implementasi abstract method 
+    @Override
+    public void move() {
+        // Implementasi move untuk zombie
+        System.out.println(getName() + " moves slowly with speed " + getSpeed());
+    }
+
+    @Override
+    public void attack() {
+        // Implementasi attack untuk zombie
+        System.out.println(getName() + " attack with " + getAttackDamage());
+    }
+}
+
