57a2ce099acbc4b96f25223fceb893ec948664fb
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2024-05-04 16:40:50 +0700

    abstract class zombie

--- app/src/main/java/Zombies/Zombie.java
+++ app/src/main/java/Zombies/Zombie.java
@@ -1,5 +1,41 @@
-package Zombies;
+package app.src.main.java.Zombies;
+import app.src.main.java.Creature.*;
 
-public abstract class Zombie {
+public abstract class Zombie extends Creature {
+    private double slowed;
+    private double speed;
+
+    public Zombie(String name, int health, int attackDamage, double attackSpeed, boolean isAquatic, double slowed, double speed) {
+        super(name, health, attackDamage, attackSpeed, isAquatic);
+        this.slowed = slowed;
+        this.speed = speed;
+    }
+
+    // Getter methods 
+    public double getSlowed() {
+        return slowed;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setAquatics(boolean air){
         
+    }
+    
+    // Abstract methods 
+    public abstract void attack();
+
+    @Override
+    public void resetCooldown(){
+        // Implementing the resetCooldown from creature
+        System.out.println(getName() + " attacks with " + getAttackDamage() + " damage!");
+    }
+
+    @Override
+    public void move() {
+        // Implementing the movement behavior from creature
+        System.out.println(getName() + " moves slowly with speed " + getSpeed());
+    }
 }
