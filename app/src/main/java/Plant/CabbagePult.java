7c2460f3490ca52241320bd0cb5cb5c036c04619
firsaaa <firsa.raissa@gmail.com>
2024-05-07 00:20:50 +0700

    update cabbage

--- app/src/main/java/Plant/CabbagePult.java
+++ app/src/main/java/Plant/CabbagePult.java
@@ -1,28 +1,28 @@
 package app.src.main.java.Plant;
 import app.src.main.java.Creature.*;
 
-public class Chomper extends Plant {
-    public Chomper(String name, int cost, int health, int attackDamage, double attackSpeed, int range, double cooldown, boolean isAquatic) {
-        super("Chomper", 150, 300, 100, 2, 1, 10, false);
+public class CabbagePult extends Plant {
+    public CabbagePult(String name, int cost, int health, int attackDamage, double attackSpeed, int range, double cooldown, boolean isAquatic) {
+        super("Cabbage Pult", 100, 100, 20, 1.5, 3, 5, false);
     }
 
     // Implementing abstract method dari Plant class
     @Override
     public void attack() {
-        // Chomper menyerang dengan memakan zombies
+        // Cabbage Pult menyerang dengan memakan zombies
         System.out.println(getName() + " nyam! chomps down on zombies!");
     }
 
     // Implementing abstract method dari Creature class
     @Override
     public void move() {
-        // Chomper tidak moving, jadi method tidak melakukan apapun
+        // Cabbage Pult tidak moving, jadi method tidak melakukan apapun
     }
 
     // Implementing abstract method dari Creature class
     @Override
     public void resetCooldown() {
-        // Chomper tidak cooldowns, jadi method tidak melakukan apapun
+        // Cabbage Pult tidak cooldowns, jadi method tidak melakukan apapun
     }
 }
 
