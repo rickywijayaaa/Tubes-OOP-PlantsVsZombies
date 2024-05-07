bf3a2f5e372897784a3987a176ea41ecac0f3a52
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2024-05-07 19:12:36 +0700

    buat tile

---
+++ app/src/main/java/MapGame/BaseTile.java
@@ -0,0 +1,7 @@
+package app.src.main.java.MapGame;
+
+public class BaseTile extends Tile {
+    public BaseTile(){
+        super("Base",false,false);
+    }
+}
---
+++ app/src/main/java/MapGame/GrassTile.java
@@ -0,0 +1,7 @@
+package app.src.main.java.MapGame;
+
+public class GrassTile extends Tile {
+    public GrassTile() {
+        super("Grass", false, false);
+    }
+}
