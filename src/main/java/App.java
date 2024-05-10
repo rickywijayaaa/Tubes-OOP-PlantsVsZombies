f32faaba995f9c7822baba8b93791f0cd59b6c14
rickywijayaaa <rickywijaya2004@gmail.com>
2024-05-10 16:17:24 +0700

    creature dibuat pake atribut koordinat deh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,21 +7,23 @@ import MapGame.*;
 import Sun.*;
 import Plant.*;
 import Zombies.*;
+import Koordinat.*;
+
+import java.util.concurrent.*;
 
 
 public class App {
     public static void main(String[] args) {
-
-        NormalZombie zom1 = new NormalZombie();
-        ConeheadZombie zom2 = new ConeheadZombie();
-        PoleVaultingZombie zom3 = new PoleVaultingZombie();
-        BucketheadZombie zom4 = new BucketheadZombie();
-        DuckyTubeZombie zom5 = new DuckyTubeZombie();
-        DolphinRiderZombie zom6 = new DolphinRiderZombie();
-        SnorkelZombie zom7 = new SnorkelZombie();
-        FlagZombie zom8 = new FlagZombie();
-        ScreendoorZombie zom9 = new ScreendoorZombie();
-        FootballZombie zom10 = new FootballZombie();
+        NormalZombie zom1 = new NormalZombie(1,1);
+        ConeheadZombie zom2 = new ConeheadZombie(1,1);
+        PoleVaultingZombie zom3 = new PoleVaultingZombie(1,1);
+        BucketheadZombie zom4 = new BucketheadZombie(1,1);
+        DuckyTubeZombie zom5 = new DuckyTubeZombie(1,1);
+        DolphinRiderZombie zom6 = new DolphinRiderZombie(1,1);
+        SnorkelZombie zom7 = new SnorkelZombie(1,1);
+        FlagZombie zom8 = new FlagZombie(1,1);
+        ScreendoorZombie zom9 = new ScreendoorZombie(1,1);
+        FootballZombie zom10 = new FootballZombie(1,1);
         zom1.displayZombie();
         zom2.displayZombie();
         zom3.displayZombie();
@@ -33,17 +35,21 @@ public class App {
         zom9.displayZombie();
         zom10.displayZombie();
         System.out.println(" ");
-        Sunflower pl1 = new Sunflower();
-        Peashooter pl2 = new Peashooter();
-        Wallnut pl3 = new Wallnut();
-        SnowPea pl4 = new SnowPea();
-        Squash pl5 = new Squash();
-        Lilypad pl6 = new Lilypad();
-        CherryBomb pl7 = new CherryBomb();
-        Repeater pl8 = new Repeater();
-        CabbagePult pl9 = new CabbagePult();
-        TangleKelp pl10 = new TangleKelp();
-
+        Sunflower pl1 = new Sunflower(1,1);
+        Peashooter pl2 = new Peashooter(1,1);
+        Wallnut pl3 = new Wallnut(1,1);
+        SnowPea pl4 = new SnowPea(1,1);
+        Squash pl5 = new Squash(1,1);
+        Lilypad pl6 = new Lilypad(1,1);
+        CherryBomb pl7 = new CherryBomb(1,1);
+        Repeater pl8 = new Repeater(1,1);
+        CabbagePult pl9 = new CabbagePult(1,1);
+        TangleKelp pl10 = new TangleKelp(1,1);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         pl1.displayPlant();
         pl2.displayPlant();
         pl3.displayPlant();
@@ -82,11 +88,19 @@ public class App {
         System.out.println("GameMap after remove entities:");
         koor11.removeCreature(pl10);
         peta.displayMap();
-        Sun matahari = new Sun();
+        Sun.getInstance();
+        Sun.setSun(25);
+
+        System.out.println("jumlah matahari : "+ Sun.getSun());
         // matahari.SunGenerate();
         // System.out.println("Total Sun: " + matahari.getSun());
         //coba desktop
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+        System.out.println("detik : " + seconds);
+        System.out.println("detik zombie : " + zom1.getStartTime());
 
+        System.out.println("Lokasi zom1 : ");
+        zom1.displayCreaturekoor();
         
     }
 }
