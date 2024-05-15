163afc629bf8a07c2a9598b574542e5e6d5ad92e
rickywijayaaa <rickywijaya2004@gmail.com>
2024-05-15 12:11:36 +0700

    update peta dari 6x9  jadi 6x11

--- src/main/java/MapGame/Peta.java
+++ src/main/java/MapGame/Peta.java
@@ -5,98 +5,92 @@ import Plant.*;
 import java.util.ArrayList;
 import Sun.*;
 
-
 public class Peta {
     private Tile[][] grid;
 
     public Peta() {
-        this.grid = new Tile[6][9];
+        this.grid = new Tile[6][11];
 
         for (int row = 0; row < 6; row++) {
-            for (int col = 0; col < 9; col++) {
+            for (int col = 0; col < 11; col++) {
                 if (col == 0) {
                     grid[row][col] = new BaseTile();
-                }
-                else if (col == 8) {
+                } else if (col == 10) {
                     grid[row][col] = new ZombieSpawnTile();
-                }
-                else if (row >= 2 && row <= 3) {
+                } else if (row >= 2 && row <= 3 && col != 0 && col!=10) {
                     grid[row][col] = new PoolTile(false);
-                }
-                else {
-                    grid[row][col] = new ZombieSpawnTile();
+                } else {
+                    grid[row][col] = new GrassTile();
                 }
             }
         }
     }
 
-    void plant(int row,int col,Plant toBePlanted) throws Exception {
-        if(col <= 0 || col == 8){
+    void plant(int row, int col, Plant toBePlanted) throws Exception {
+        if (col <= 0 || col >= 10) {
             throw new Exception("Tile bertanam invalid");
         }
-        if(Sun.getSun() < toBePlanted.getCost() ){
+        if (Sun.getSun() < toBePlanted.getCost()) {
             throw new Exception("Tidak cukup matahari untuk membeli tanaman");
         }
-        if(toBePlanted.isInCooldown()){
+        if (toBePlanted.isInCooldown()) {
             throw new Exception("Tanaman " + toBePlanted.getName() + " masih dalam cooldown");
         }
 
         boolean pool = row >= 2 && row <= 3;
-        if(toBePlanted instanceof Lilypad && !pool){
+        if (toBePlanted instanceof Lilypad && !pool) {
             throw new Exception("Tile bertanam invalid");
         }
 
         boolean lilyPlanted = false;
         boolean plantOnTop = false;
-        for(Creature creature : grid[row][col].getEntities()){
-            if(creature instanceof  Lilypad){
+        for (Creature creature : grid[row][col].getEntities()) {
+            if (creature instanceof Lilypad) {
                 lilyPlanted = true;
-            } else if(creature.getClass().isAssignableFrom(Plant.class)){
+            } else if (creature.getClass().isAssignableFrom(Plant.class)) {
                 plantOnTop = true;
             }
         }
 
-        if(pool && !lilyPlanted  && !(toBePlanted instanceof Lilypad)){
+        if (pool && !lilyPlanted && !(toBePlanted instanceof Lilypad)) {
             throw new Exception("Tile bertanam invalid");
         }
 
-        if(plantOnTop){
-            throw  new Exception("Tile bertanam invalid");
+        if (plantOnTop) {
+            throw new Exception("Tile bertanam invalid");
         }
 
         toBePlanted.setCooldown(true);
         this.grid[row][col].addCreature(toBePlanted);
     }
 
-public Tile getTile(int row, int col) {
-    if (row < 0 || row > 6 || col < 0 || col > 9) {
-        //throw exception
-    }
-    return this.grid[row][col];
-    }
-
-public void displayMap() {
-    for (int row = 0; row < 6; row++) {
-        for (int col = 0; col < 9; col++) {
-            Tile tile = getTile(row, col);
-            ArrayList<Creature> entities = tile.getEntities();
-                
-            StringBuilder sb = new StringBuilder();
-            sb.append("[");
-            for (Creature entity : entities) {
-                sb.append(entity.getName()).append(", ");
-            }
-
-            if (sb.length() > 1) { 
-                sb.setLength(sb.length() - 2); 
-            }
-
-            sb.append("]");
-            System.out.print(sb.toString());
+    public Tile getTile(int row, int col) {
+        if (row < 0 || row >= 6 || col < 0 || col >= 11) {
+            throw new IllegalArgumentException("Indeks di luar batas matriks");
         }
-        System.out.println();
+        return this.grid[row][col];
     }
-}
 
-    
+    public void displayMap() {
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 11; col++) {
+                Tile tile = getTile(row, col);
+                ArrayList<Creature> entities = tile.getEntities();
+
+                StringBuilder sb = new StringBuilder();
+                sb.append("[");
+                for (Creature entity : entities) {
+                    sb.append(entity.getName()).append(", ");
+                }
+
+                if (sb.length() > 1) { 
+                    sb.setLength(sb.length() - 2); 
+                }
+
+                sb.append("]");
+                System.out.print(sb.toString());
+            }
+            System.out.println();
+        }
+    }
 }
