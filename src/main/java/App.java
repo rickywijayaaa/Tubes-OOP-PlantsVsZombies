a44bff6e8f414b8d8a99a0d19507194f01385a8e
rickywijayaaa <rickywijaya2004@gmail.com>
2024-05-18 12:43:59 +0700

    update zombie spawn thread ditambahin plant attack

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -37,14 +37,45 @@ public class App {
         // sunThread.addSun(sunflower2);
         // sunThread.addSun(sunflower3);
         // sunThread.addSun(sunflower4);
+
+        // Repeater sun1 = new Repeater(0, 1);
+        // CabbagePult sun2 = new CabbagePult(1, 1);
+        // Peashooter plant2 = new Peashooter(4, 1);
+        // SnowPea plant3 = new SnowPea(5, 1);
+        // Tile zomtile5 = peta.getTile(0, 1);
+        // Tile zomtile6 = peta.getTile(1, 1);
+        // Tile zomtile7 = peta.getTile(4, 1);
+        // Tile zomtile8 = peta.getTile(5, 1);
+        // zomtile5.addCreature(sun1);
+        // zomtile6.addCreature(sun2);
+        // zomtile7.addCreature(plant2);
+        // zomtile8.addCreature(plant3);
+
+        // sun1.attack2(peta);
+        // sun2.attack2(peta);
+        // plant2.attack2(peta);
+        // plant3.attack2(peta);
+
+        // NormalZombie zom1 = new NormalZombie(1,10);
+        // ConeheadZombie zom2 = new ConeheadZombie(4,10);
+        // Tile zomtil5 = peta.getTile(1, 10);
+        // Tile zomtil6 = peta.getTile(4, 10);
+        // zomtil5.addCreature(zom1);
+        // zomtil6.addCreature(zom2);
+        // sun1.attack2(peta);
+        // sun2.attack2(peta);
+        // plant2.attack2(peta);
+        // plant3.attack2(peta);
+       //peta.displayMap();
+
         
         // Start the GenerateSunThread
         // Thread thread = new Thread(sunThread);
         // thread.start();
-        //MenuGame.Menu();
         ThreadControl.addThread(new GenerateSunThread(100));
         ThreadControl.addThread(new GameTimerThread(0));
         ThreadControl.addThread(new ZombieSpawnThread(200,peta));
-        ThreadControl.startAllThreads();
+        ThreadControl.startAllThreads();        //MenuGame.Menu();
+
     }       
 }
