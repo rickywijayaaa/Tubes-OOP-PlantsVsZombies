71b82eba174b8ae6abe134228a324b48c5834f2d
stdfreesince03 <winatatristan04@gmail.com>
2024-05-19 15:02:05 +0700

    "delete misc.xml"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 
 
-public class App {
+public class  App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isRunningGame = true;
